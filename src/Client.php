6fa35484c2034d4ab5819a8e0fbf859787d5053a
a.kalin <maddmages@gmail.com>
2021-05-02 04:37:46 +0300

    updated signature

--- src/Client.php
+++ src/Client.php
@@ -71,7 +71,7 @@ class Client extends TypedClient
      * @return string Returned json string
      * @throws Throwable
      */
-    protected function _rawApiCall(string $method, array $parameters, bool $withFiles = false): string
+    public function _apiCall(string $method, array $parameters, bool $withFiles = false): string
     {
         if ($withFiles) {
             $multipart = [];
